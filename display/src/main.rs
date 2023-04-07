0cb2d6de12483b0ee828d39e6a720cafccacbf85
eloylp <eloylp@eloylp.dev>
2023-04-08 00:33:22 +0200

    Add initial project for display example

---
+++ display/src/main.rs
@@ -0,0 +1,61 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use hd44780_driver::{Cursor, CursorBlink, Display, DisplayMode, HD44780};
+
+use hal::{
+    clock::ClockControl, i2c::I2C, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
+    Rtc, IO,
+};
+
+const I2C_ADDRESS: u8 = 0x20;
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.SYSTEM.split();
+
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
+
+    // disable watchdog timers
+    rtc.swd.disable();
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio0,
+        io.pins.gpio1,
+        400u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+
+    let mut delay = Delay::new(&clocks);
+
+    let mut lcd = HD44780::new_i2c(i2c, I2C_ADDRESS, &mut delay).unwrap();
+    lcd.reset(&mut delay).unwrap();
+    lcd.clear(&mut delay).unwrap();
+    lcd.set_display_mode(
+        DisplayMode {
+            display: Display::On,
+            cursor_visibility: Cursor::Visible,
+            cursor_blink: CursorBlink::On,
+        },
+        &mut delay,
+    )
+    .unwrap();
+    let _ = lcd.write_str("Hello, world!", &mut delay).unwrap();
+
+    loop {}
+}
